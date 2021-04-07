a5e2adf066592d8fe09be1668aaee15b709914be
Max Demenko <max.demenko@gmail.com>
2021-04-07 18:37:49 +0300

    add system properties to project

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -15,11 +15,12 @@ public class TestBase {
     static void setup() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.startMaximized = true;
+
+        //settings for remote run
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 
     @AfterEach
