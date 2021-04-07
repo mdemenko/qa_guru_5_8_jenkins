59279d3bfd1bdc66ee259318e6c6d2f7e7e94fbc
Max Demenko <max.demenko@gmail.com>
2021-04-07 11:30:22 +0300

    add test for further testing

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,12 +3,30 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static helpers.AttachmentHelper.*;
 
 public class TestBase {
     @BeforeAll
     static void setup() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.startMaximized = true;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+    }
+
+    @AfterEach
+    void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachVideo();
+        closeWebDriver();
     }
 }
